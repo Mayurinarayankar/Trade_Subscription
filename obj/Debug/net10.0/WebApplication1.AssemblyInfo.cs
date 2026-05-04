19dcb6be7a7616ec59f806f9e040e57ae5627603
Mayuri Narayankar <mayuri.narayankar@webitoinfotech.com>
2026-05-04 21:52:53 +0530

    feat: Dll files

--- obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7b51caa854ba80dd190f2e6dbe42cfc260a3ced")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a707059417e68de4915c5503b03a40eba502311")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
