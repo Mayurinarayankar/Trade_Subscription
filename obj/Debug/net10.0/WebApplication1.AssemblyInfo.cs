6713a291e5b887af10dccd161b04a47340f2b1cb
Mayuri Narayankar <mayuri.narayankar@webitoinfotech.com>
2026-05-04 15:45:33 +0530

    feat: added the interfaces

--- obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3cc85b6c95d630aea256fd03356823dd87ac725")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79028e98452e24bf51020df50d615c77165f1df9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
