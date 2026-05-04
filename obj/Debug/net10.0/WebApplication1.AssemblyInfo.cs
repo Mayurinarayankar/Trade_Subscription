6ea4f5ea17b3db62bb6321435e70a67908a5cdec
Mayuri Narayankar <mayuri.narayankar@webitoinfotech.com>
2026-05-04 22:03:56 +0530

    feat: added the connection string and the swagger, exceptionMiddleware

--- obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a707059417e68de4915c5503b03a40eba502311")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19dcb6be7a7616ec59f806f9e040e57ae5627603")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
